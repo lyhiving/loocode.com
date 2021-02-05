46099f58e12bcc34c81eda6b3c758c447e2ea7ed
zhoutianliang <mfkgdyve@gmail.com>
2021-02-05 15:49:04 +0800

    mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,30 +23,30 @@ mix.webpackConfig({
     }
 });
 
-mix.setPublicPath('public');
+mix.setPublicPath('dist');
 
 //合并文件，分离库和页面JS
-mix.js('resources/js/app.js', "public/assets/js/app.js")
-    .js('resources/js/posts.js', "public/assets/js/article.js")
-    .js('resources/js/user.js', "public/assets/js/user.js")
+mix.js('resources/js/app.js', "dist/assets/js/app.js")
+    .js('resources/js/posts.js', "dist/assets/js/article.js")
+    .js('resources/js/user.js', "dist/assets/js/user.js")
     .styles([
         "node_modules/bootstrap/dist/css/bootstrap.css",
         "node_modules/social-share.js/dist/css/share.min.css",
         "resources/css/app.css",
     ], "public/assets/css/app.css")
-    .css("resources/css/article.css", "public/assets/css/article.css")
+    .css("resources/css/article.css", "dist/assets/css/article.css")
     .styles([
         "node_modules/filepond/dist/filepond.css",
-    ], "public/assets/css/filepond.css")
+    ], "dist/assets/css/filepond.css")
     .copyDirectory(
         ['resources/images/'],
-        'public/assets/images/'
+        'dist/assets/images/'
     )
     .copyDirectory(
         [
             "node_modules/social-share.js/dist/fonts",
         ],
-        'public/assets/fonts/'
+        'dist/assets/fonts/'
     )
     .extract(['jquery', 'popper.js', 'bootstrap', 'superagent', 'social-share.js'])
     .autoload({
