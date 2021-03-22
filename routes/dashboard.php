e9b900bcb5612f1910308cb858af0f8ef85347b6
zhoutianliang <mfkgdyve@gmail.com>
2021-03-22 15:33:05 +0800

    prop

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -26,9 +26,9 @@ Route::post('/authorize/register', [AuthorizeController::class, 'register']);
 Route::post('/authorize/login', [AuthorizeController::class, 'authenticate']);
 Route::post('/authorize/logout', [AuthorizeController::class, 'logout']);
 Route::get('/open/configuration', [OpenController::class, 'configuration']);
-Route::get('/dashboard', [DashboardController::class, 'main']);
 
 Route::middleware(['auth:backend', 'rbac'])->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'main']);
     Route::any('/ckfinder/connector', [CKFinderController::class, 'request']);
     Route::get('/open/user/menu', [OpenController::class, 'userMenu']);
     Route::get('/open/menus', [OpenController::class, 'menus']);
